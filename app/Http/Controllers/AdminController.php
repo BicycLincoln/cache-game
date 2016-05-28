13696a0c0432f5390a0946c5db317d5646f18ae5
Derek J. Augustine <derek.augustine@archrival.com>
2016-05-28 07:03:50 -0500

    working in auth stuff

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,5 +8,13 @@ use App\Http\Requests;
 
 class AdminController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function getIndex()
+    {
+        
+    }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,28 +1,42 @@
 <?php
 
-Route::get('/', ['as' => 'get.index', 'uses' => 'PublicController@getIndex']);
-Route::any('/robots.txt', ['as' => 'get.robots', 'uses' => 'PublicController@anyRobots']);
-Route::get('/cache/{id}', ['as' => 'get.cache', 'uses' => 'PublicController@getCache']);
-Route::get('/claim/{code}', ['as' => 'get.claim', 'uses' => 'PublicController@getClaim']);
-Route::post('/claim/{code}', ['as' => 'post.claim', 'uses' => 'PublicController@postClaim']);
+Route::get('/', ['as' => 'index.get', 'uses' => 'PublicController@getIndex']);
+Route::any('/robots.txt', ['as' => 'robots.any', 'uses' => 'PublicController@anyRobots']);
+Route::get('/cache/{id}', ['as' => 'cache.get', 'uses' => 'PublicController@getCache']);
+Route::get('/claim/{code}', ['as' => 'claim.get', 'uses' => 'PublicController@getClaim']);
+Route::post('/claim/{code}', ['as' => 'claim.post', 'uses' => 'PublicController@postClaim']);
 
-//Route::get('/', 'Hom')
-//Route::get('/', function () {
-//    $size = 2048;
-//    $code = new TCPDF2DBarcode('http://www.bicyclincoln.org/', 'QRCODE,H');
-//    $cols = intval($code->getBarcodeArray()['num_cols']);
-//    $unit_size = ceil($size / $cols);
-//    $size = $cols * $unit_size;
-//    $image = Image::make($code->getBarcodePngData($unit_size, $unit_size, [0, 0, 0]));
-//
-//    $background = Image::canvas($size, $size, '#FFFFFF');
-//
-//    $background->insert($image, 'center');
-//
-//    $icon = Image::make(storage_path('assets/qr-logo.png'));
-//    $icon->resize($size, $size);
-//
-//    $background->insert($icon, 'center');
-//
-//    return $background->response('png');
-//});
+Route::group(
+    ['prefix' => '/admin'],
+    function () {
+        Route::get('/', ['as' => 'admin.index.get', 'uses' => 'AdminController@getIndex']);
+    }
+);
+
+Route::group(
+    ['prefix' => '/develop'],
+    function () {
+        Route::get('/qr', function () {
+            $size = 2048;
+            $code = new TCPDF2DBarcode('http://www.bicyclincoln.org/', 'QRCODE,H');
+            $cols = intval($code->getBarcodeArray()['num_cols']);
+            $unit_size = ceil($size / $cols);
+            $size = $cols * $unit_size;
+            $image = Image::make($code->getBarcodePngData($unit_size, $unit_size, [0, 0, 0]));
+
+            $background = Image::canvas($size, $size, '#FFFFFF');
+
+            $background->insert($image, 'center');
+
+            $icon = Image::make(storage_path('assets/qr-logo.png'));
+            $icon->resize($size, $size);
+
+            $background->insert($icon, 'center');
+
+            return $background->response('png');
+        });
+    }
+);
+
+
+Route::auth();
